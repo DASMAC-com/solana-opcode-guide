b67243d0a833dab1a2578e51c1a65f992cd20f85
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2025-11-14 15:41:20 -0800

    [ENG-75] Add `hello-dasmac` example (#9)
    * Add preliminary example scaffolding
    
    * COmment on file
    
    * Add quickstart page
    
    * Update to hello DASMAC

---
+++ examples/src/lib.rs
@@ -0,0 +1,20 @@
+#[cfg(test)]
+mod tests {
+    use mollusk_svm::{result::Check, Mollusk};
+    use solana_sdk::instruction::Instruction;
+    use solana_sdk::pubkey::{Pubkey, PUBKEY_BYTES};
+
+    #[test]
+    fn test_hello_dasmac() {
+        let program_id_keypair_bytes = std::fs::read("deploy/examples-keypair.json").unwrap()
+            [..PUBKEY_BYTES]
+            .try_into()
+            .expect("slice with incorrect length");
+        let program_id = Pubkey::new_from_array(program_id_keypair_bytes);
+        let instruction = Instruction::new_with_bytes(program_id, &[], vec![]);
+        let mollusk = Mollusk::new(&program_id, "deploy/hello-dasmac");
+        let result =
+            mollusk.process_and_validate_instruction(&instruction, &[], &[Check::success()]);
+        assert!(!result.program_result.is_err());
+    }
+}
