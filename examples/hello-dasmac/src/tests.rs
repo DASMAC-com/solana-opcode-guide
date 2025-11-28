317ad3b8021d4877e55e9512ef28cd487197867b
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2025-11-28 11:18:46 -0800

    Clean up test layout

---
+++ examples/hello-dasmac/src/tests.rs
@@ -0,0 +1,16 @@
+use mollusk_svm::{result::Check, Mollusk};
+use solana_sdk::instruction::Instruction;
+use solana_sdk::signature::read_keypair_file;
+use solana_sdk::signer::Signer;
+
+#[test]
+fn asm() {
+    let keypair =
+        read_keypair_file("deploy/hello-dasmac-keypair.json").expect("Failed to read keypair file");
+    let program_id = keypair.pubkey();
+
+    let instruction = Instruction::new_with_bytes(program_id, &[], vec![]);
+    let mollusk = Mollusk::new(&program_id, "deploy/hello-dasmac");
+    let result = mollusk.process_and_validate_instruction(&instruction, &[], &[Check::success()]);
+    assert!(!result.program_result.is_err());
+}
