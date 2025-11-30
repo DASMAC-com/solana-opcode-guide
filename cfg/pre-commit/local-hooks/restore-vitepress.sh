a90d045a649a9e113d8b9e647516b513c5d83372
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2025-11-29 17:41:53 -0800

    Run sh formatting

--- cfg/pre-commit/local-hooks/restore-vitepress.sh
+++ cfg/pre-commit/local-hooks/restore-vitepress.sh
@@ -2,10 +2,10 @@
 # Restore VitePress syntax corrupted by markdown formatters.
 set -e
 for file in "$@"; do
-  if [[ "$file" == docs/src/* ]] && [[ "$file" == *.md ]]; then
-    # Replace `\<<<` with `<<<`
-    sed -i '' 's/\\<<</<<</g' "$file"
-    # Replace `\{` with `{`
-    sed -i '' 's/\\{/{/g' "$file"
-  fi
+	if [[ "$file" == docs/src/* ]] && [[ "$file" == *.md ]]; then
+		# Replace `\<<<` with `<<<`
+		sed -i '' 's/\\<<</<<</g' "$file"
+		# Replace `\{` with `{`
+		sed -i '' 's/\\{/{/g' "$file"
+	fi
 done
