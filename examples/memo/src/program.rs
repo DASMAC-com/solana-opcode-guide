59f75dc4ea30adb68b7aece3d96dc69da6183684
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2025-12-02 10:09:05 -0800

    Update Rust program to match assembly

--- examples/memo/src/program.rs
+++ examples/memo/src/program.rs
@@ -1,15 +1,19 @@
-use pinocchio::{entrypoint, msg, pubkey::Pubkey, ProgramResult};
+use pinocchio::{entrypoint, pubkey::Pubkey, ProgramResult};
+
+#[cfg(target_os = "solana")]
+use pinocchio::syscalls::sol_log_;
 
 entrypoint!(process_instruction);
 
+#[cfg_attr(not(target_os = "solana"), allow(unused_variables))]
 fn process_instruction(
     _program_id: &Pubkey,
     _accounts: &[pinocchio::account_info::AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    // Log the instruction data (assumes valid UTF-8 bytes).
+    #[cfg(target_os = "solana")]
     unsafe {
-        msg!(core::str::from_utf8_unchecked(instruction_data));
+        sol_log_(instruction_data.as_ptr(), instruction_data.len() as u64);
     }
     Ok(())
 }
