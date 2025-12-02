6f4372d82dc11c26343fa3b6bb27cb7dcd78b50f
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2025-12-02 10:14:03 -0800

    Use `#` for comments so VitePress formats them

--- examples/memo/src/memo/memo.s
+++ examples/memo/src/memo/memo.s
@@ -5,18 +5,18 @@
 .globl entrypoint
 
 entrypoint:
-    // Indexed load the number of accounts into the return code.
+    # Indexed load the number of accounts into the return code.
     ldxdw r0, [r1 + NUM_ACCOUNTS_OFFSET]
-    // If nonzero number of accounts, jump to exit instruction.
+    # If nonzero number of accounts, jump to exit instruction.
     jne r0, r4, 3
-    // Indexed load the message data length.
+    # Indexed load the message data length.
     ldxdw r2, [r1 + INSTRUCTION_DATA_LENGTH_OFFSET]
-    // Increment pointer in r1 the instruction data offset.
+    # Increment pointer in r1 the instruction data offset.
     add64 r1, INSTRUCTION_DATA_OFFSET
     call sol_log_
     exit
 
-// Without mock .rodata the dump script fails.
-// https://github.com/blueshift-gg/sbpf/issues/82
+# Without mock .rodata the dump script fails.
+# https://github.com/blueshift-gg/sbpf/issues/82
 .rodata
     null: .byte 0
