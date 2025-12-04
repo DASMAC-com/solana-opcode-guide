e88abb7c9eeef9ac74312eeaa01cc31d533622b6
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2025-12-03 17:03:21 -0800

    ci(ENG-110): Add `prettier` docs build support

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,19 +1,17 @@
 export default {
-  title: 'Solana Opcode Guide',
-  srcDir: 'src',
+  title: "Solana Opcode Guide",
+  srcDir: "src",
   themeConfig: {
     sidebar: [
-      { text: 'Welcome', link: '/' },
-      { text: 'Quickstart', link: '/quickstart' },
+      { text: "Welcome", link: "/" },
+      { text: "Quickstart", link: "/quickstart" },
       {
         collapsed: false,
-        text: 'Examples',
-        link: '/examples/',
-        items: [
-          { text: 'Memo', link: '/examples/memo' }
-        ]
+        text: "Examples",
+        link: "/examples/",
+        items: [{ text: "Memo", link: "/examples/memo" }],
       },
-      { text: 'Resources', link: '/resources' }
-    ]
-  }
-}
+      { text: "Resources", link: "/resources" },
+    ],
+  },
+};
