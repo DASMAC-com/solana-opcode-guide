9dc919566ad87f8d3e1d4de389202ce70e380011
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2025-12-08 18:24:57 +0400

    Update sh layout

--- .github/actions/install-toolchain/scripts/cache-deps.sh
+++ .github/actions/install-toolchain/scripts/cache-deps.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 set -e
-SBPF_ARCH_A="$1" TOOLS_VERSION_A="$2" SBPF_ARCH_B="$3" TOOLS_VERSION_B="$4"
+SBPF_ARCH_A="$1"
+TOOLS_VERSION_A="$2"
+SBPF_ARCH_B="$3"
+TOOLS_VERSION_B="$4"
+
 cd examples
 cargo test --no-run
-(cd utils/program-deps && cargo build-sbf --arch "$SBPF_ARCH_A" --tools-version "$TOOLS_VERSION_A")
-(cd utils/program-deps && cargo build-sbf --arch "$SBPF_ARCH_B" --tools-version "$TOOLS_VERSION_B")
+
+(
+	cd utils/program-deps &&
+		cargo build-sbf --arch "$SBPF_ARCH_A" --tools-version "$TOOLS_VERSION_A" &&
+		cargo build-sbf --arch "$SBPF_ARCH_B" --tools-version "$TOOLS_VERSION_B"
+)
