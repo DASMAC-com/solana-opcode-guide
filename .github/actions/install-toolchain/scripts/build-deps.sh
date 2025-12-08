726ba35b67055f871c284099a676ce84961a17a1
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2025-12-08 22:42:00 +0400

    Update dir switching

--- .github/actions/install-toolchain/scripts/build-deps.sh
+++ .github/actions/install-toolchain/scripts/build-deps.sh
@@ -5,10 +5,11 @@ TOOLS_VERSION_A="$2"
 SBPF_ARCH_B="$3"
 TOOLS_VERSION_B="$4"
 
+cd examples
 cargo build --package build-deps
 cargo test --no-run --package build-deps
 (
-	cd utils/deps/program/program &&
+	cd utils/deps/program &&
 		cargo build-sbf --arch "$SBPF_ARCH_A" --tools-version "$TOOLS_VERSION_A" &&
 		cargo build-sbf --arch "$SBPF_ARCH_B" --tools-version "$TOOLS_VERSION_B"
 )
