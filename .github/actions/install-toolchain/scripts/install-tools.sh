fd1c56ddfa206ce83b7d1aa57faa2be74d4b0c51
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2025-12-08 18:53:07 +0400

    Update version passing

--- .github/actions/install-toolchain/scripts/install-tools.sh
+++ .github/actions/install-toolchain/scripts/install-tools.sh
@@ -2,14 +2,16 @@
 set -e
 
 SOLANA_VERSION="$1"
-PLATFORM_TOOLS_VERSION="$2"
-SBPF_REVISION="$3"
+PLATFORM_TOOLS_VERSION_DUMP="$2"
+PLATFORM_TOOLS_VERSION_TEST="$3"
+SBPF_REVISION="$4"
 
 # Install Solana toolchain.
 sh -c "$(curl -sSfL "https://release.anza.xyz/${SOLANA_VERSION}/install")"
 
 # Install platform-tools.
-cargo-build-sbf --install-only --tools-version "$PLATFORM_TOOLS_VERSION"
+cargo-build-sbf --install-only --tools-version "$PLATFORM_TOOLS_VERSION_DUMP"
+cargo-build-sbf --install-only --tools-version "$PLATFORM_TOOLS_VERSION_TEST"
 
 # Run platform tools SBF install script (cargo-build-sbf skips
 # this at install time, so it cache misses at build time).
