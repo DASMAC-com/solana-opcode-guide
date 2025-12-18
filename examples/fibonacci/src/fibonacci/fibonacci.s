2ca656b58e55e92153448aa2b8cf58c795bf68ea
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2025-12-17 17:33:17 -0800

    Comment out what doesn't build

--- examples/fibonacci/src/fibonacci/fibonacci.s
+++ examples/fibonacci/src/fibonacci/fibonacci.s
@@ -18,40 +18,40 @@ entrypoint:
     # single byte since MAX_N < 256.
     ldxb r8, [r1 + INSTRUCTION_DATA_OFFSET]
     # If sequence number > MAX_N, jump to exit with error code E_MAX_N.
-    jgt r8, MAX_N, abort_max_n
+    #jgt r8, MAX_N, abort_max_n
 
-    # Prepare call-preserved registers for loop.
-    # r6 = F(0) = 0, r7 = F(1) = 1. (r6 defaults to 0).
-    mov64 r7, 1
+#    # Prepare call-preserved registers for loop.
+#    # r6 = F(0) = 0, r7 = F(1) = 1. (r6 defaults to 0).
+#    mov64 r7, 1
 
-    # F(n) = n for n = 0, 1. So compare sequence number to
-    # MAX_N_SPECIAL_CASE then loop if not special case.
-    jgt r8, MAX_N_SPECIAL_CASE, loop
-    mov64 r0, r8
+#    # F(n) = n for n = 0, 1. So compare sequence number to
+#    # MAX_N_SPECIAL_CASE then loop if not special case.
+#    jgt r8, MAX_N_SPECIAL_CASE, loop
+#    mov64 r0, r8
     exit
 
-loop:
+#loop:
     # Decrement sequence number tracker for iteration.
     # Using r9 as a temporary register, increment the sequence
     # numbers of the two Fibonacci numbers being tracked.
     # e.g. on the first iteration,
     # r6 = F(0), r7 = F(1) -> r6 = F(1), r7 = F(2).
-    mov64 r9, r6
-    mov64 r6, r7
-    add64 r7, r9
+#    mov64 r9, r6
+#    mov64 r6, r7
+#    add64 r7, r9
 
-    # Decrement sequence number counter.
-    sub32 r8, 1
-    # If sequence number counter > 1, continue loop.
-    jgt r8, MAX_N_SPECIAL_CASE, loop
-    # Now result in r7 = F(n), move into return code register.
-    mov64 r0, r7
-    exit
+#    # Decrement sequence number counter.
+#    sub32 r8, 1
+#    # If sequence number counter > 1, continue loop.
+#    jgt r8, MAX_N_SPECIAL_CASE, loop
+#    # Now result in r7 = F(n), move into return code register.
+#    mov64 r0, r7
+#    exit
 
 abort_accounts:
     sub64 r0, E_ACCOUNTS
     exit
 
-abort_max_n:
-    sub64 r0, E_MAX_N
-    exit
+# abort_max_n:
+#     sub64 r0, E_MAX_N
+#     exit
