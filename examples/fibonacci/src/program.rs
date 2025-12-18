56396abe02f97b18e8f8489bd394d9904d3349fb
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2025-12-18 11:24:14 -0800

    Get cargo test to run

--- examples/fibonacci/src/program.rs
+++ examples/fibonacci/src/program.rs
@@ -1,8 +1,5 @@
 use pinocchio::{entrypoint, pubkey::Pubkey, ProgramResult};
 
-#[cfg(target_os = "solana")]
-use pinocchio::syscalls::sol_log_;
-
 entrypoint!(process_instruction);
 
 #[cfg_attr(not(target_os = "solana"), allow(unused_variables))]
@@ -11,9 +8,5 @@ fn process_instruction(
     _accounts: &[pinocchio::account_info::AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    #[cfg(target_os = "solana")]
-    unsafe {
-        sol_log_(instruction_data.as_ptr(), instruction_data.len() as u64);
-    }
-    Ok((2))
+    Ok(())
 }
