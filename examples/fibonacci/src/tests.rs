bc8f416b45b09bfd04c61ca170712f3898852644
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2025-12-18 18:10:25 -0800

    Abstract test

--- examples/fibonacci/src/tests.rs
+++ examples/fibonacci/src/tests.rs
@@ -14,17 +14,18 @@ fn fib(n: u8) -> u64 {
     Fib::single(n.into()).try_into().unwrap()
 }
 
-#[test]
-fn test_asm() {
-    let setup = setup_test(ProgramLanguage::Assembly);
+fn test_fibonacci_program(language: ProgramLanguage, test_account_error: bool) {
+    let setup = setup_test(language);
 
-    // Verify failure for passing an account.
-    let (account, accounts) = single_mock_account();
-    setup.mollusk.process_and_validate_instruction(
-        &Instruction::new_with_bytes(setup.program_id, &[], accounts.clone()),
-        &[account],
-        &[Check::err(ProgramError::Custom(E_PASSED_ACCOUNT))],
-    );
+    if test_account_error {
+        // Verify failure for passing an account.
+        let (account, accounts) = single_mock_account();
+        setup.mollusk.process_and_validate_instruction(
+            &Instruction::new_with_bytes(setup.program_id, &[], accounts.clone()),
+            &[account],
+            &[Check::err(ProgramError::Custom(E_PASSED_ACCOUNT))],
+        );
+    }
 
     // Verify failure for index too big.
     setup.mollusk.process_and_validate_instruction(
@@ -79,59 +80,13 @@ fn test_asm() {
 }
 
 #[test]
+fn test_asm() {
+    test_fibonacci_program(ProgramLanguage::Assembly, true);
+}
+
+#[test]
 fn test_rs() {
-    let setup = setup_test(ProgramLanguage::Rust);
-
-    // Verify failure for index too big.
-    setup.mollusk.process_and_validate_instruction(
-        &Instruction::new_with_bytes(setup.program_id, &[MAX_FIB_INDEX_U32 + 1], vec![]),
-        &[],
-        &[Check::err(ProgramError::Custom(E_INDEX_TOO_BIG))],
-    );
-
-    // Initialize a vector with 48 slots, one for each Fibonacci number from F(0) to F(47), along
-    // with their compute unit consumption.
-    let mut fib_numbers: Vec<(u32, u64)> = vec![(0, 0); (MAX_FIB_INDEX_U32 + 1) as usize];
-
-    // For F(0) = 0, the program result should be considered a success.
-    fib_numbers[0] = (
-        0,
-        setup
-            .mollusk
-            .process_and_validate_instruction(
-                &Instruction::new_with_bytes(setup.program_id, &[0], vec![]),
-                &[],
-                &[Check::success()],
-            )
-            .compute_units_consumed,
-    );
-
-    // For F(1) onwards, verify correct Fibonacci numbers are returned as a custom error.
-    for n in 1..=MAX_FIB_INDEX_U32 {
-        let expected_fib: u32 = fib(n) as u32;
-        fib_numbers[n as usize] = (
-            expected_fib,
-            setup
-                .mollusk
-                .process_and_validate_instruction(
-                    &Instruction::new_with_bytes(setup.program_id, &[n], vec![]),
-                    &[],
-                    &[Check::err(ProgramError::Custom(expected_fib))],
-                )
-                .compute_units_consumed,
-        );
-    }
-
-    // Pretty print the Fibonacci numbers along with their compute unit consumption.
-    println!("Fibonacci numbers and compute units:");
-    for n in 0..=MAX_FIB_INDEX_U32 {
-        println!(
-            "{:<7}{:<10} (Compute Units: {})",
-            format!("F({}):", n),
-            fib_numbers[n as usize].0,
-            fib_numbers[n as usize].1
-        );
-    }
+    test_fibonacci_program(ProgramLanguage::Rust, false);
 }
 
 /// Verify the index of the maximum Fibonacci number that fits in a u32, while allowing space for
