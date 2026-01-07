32f46951fdb3a3b43973171c80ec7da963fc5126
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2026-01-07 15:23:10 -0800

    chore(ENG-140): Add OpenGraph site tags (#33)
    Add assorted OpenGraph site tags

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,20 @@ export default {
   title: "Solana Opcode Guide",
   description:
     "Courtesy of Distributed Atomic State Machine Algorithms Corporation (DASMAC)",
+  head: [
+    ["meta", { property: "og:site_name", content: "DASMAC" }],
+    ["meta", { property: "og:type", content: "website" }],
+    ["meta", { property: "og:url", content: "https://opcodes.dasmac.com/" }],
+    ["meta", { property: "og:title", content: "Solana Opcode Guide" }],
+    [
+      "meta",
+      {
+        property: "og:description",
+        content:
+          "Courtesy of Distributed Atomic State Machine Algorithms Corporation (DASMAC)",
+      },
+    ],
+  ],
   srcDir: "src",
   markdown: {
     lineNumbers: true,
