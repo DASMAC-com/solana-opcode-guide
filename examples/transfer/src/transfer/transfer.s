0f255f6f2068b836647e3cb1c02775c543f92148
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2026-01-07 18:07:09 -0800

    Add range checks

--- examples/transfer/src/transfer/transfer.s
+++ examples/transfer/src/transfer/transfer.s
@@ -4,13 +4,15 @@
 .equ E_DUPLICATE_ACCOUNT_RECIPIENT, 2
 # System program account is a duplicate.
 .equ E_DUPLICATE_ACCOUNT_SYSTEM_PROGRAM, 3
+# Invalid instruction data length.
+.equ E_INVALID_INSTRUCTION_DATA_LENGTH, 4
 
 # Account positioning.
 .equ N_ACCOUNTS_OFFSET, 0
 .equ N_ACCOUNTS_EXPECTED, 3
 .equ NON_DUP_MARKER, 0xff
 
-# Sender accounts.
+# Sender account.
 .equ SENDER_OFFSET, 8
 
 # Recipient account.
@@ -20,7 +22,8 @@
 .equ SYSTEM_PROGRAM_OFFSET, 20680
 
 # Transfer amount.
-.equ INSTRUCTION_DATA_LEN, 31032
+.equ INSTRUCTION_DATA_LENGTH_OFFSET, 31032
+.equ INSTRUCTION_DATA_OFFSET, 31040
 
 .global entrypoint
 
@@ -35,6 +38,11 @@ entrypoint:
     ldxb r2, [r1 + SYSTEM_PROGRAM_OFFSET]
     jne r2, NON_DUP_MARKER, e_duplicate_account_system_program
 
+    # Check instruction data, storing transfer amount in r4.
+    ldxdw r4, [r1 + INSTRUCTION_DATA_LENGTH_OFFSET]
+    jne r4, 8, e_invalid_instruction_data_length
+    ldxdw r4, [r1 + INSTRUCTION_DATA_OFFSET]
+
     exit
 
 e_n_accounts:
@@ -47,4 +55,8 @@ e_duplicate_account_recipient:
 
 e_duplicate_account_system_program:
     mov32 r0, E_DUPLICATE_ACCOUNT_SYSTEM_PROGRAM
+    exit
+
+e_invalid_instruction_data_length:
+    mov32 r0, E_INVALID_INSTRUCTION_DATA_LENGTH
     exit
