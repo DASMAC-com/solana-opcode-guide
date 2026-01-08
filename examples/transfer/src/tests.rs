6dd6b219d7bcf7b96500294e37036bc7c5ad912e
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2026-01-08 15:17:37 -0800

    Fail for nonzero data length

--- examples/transfer/src/tests.rs
+++ examples/transfer/src/tests.rs
@@ -12,6 +12,14 @@ const E_DUPLICATE_ACCOUNT_RECIPIENT: u32 = 2;
 const E_DUPLICATE_ACCOUNT_SYSTEM_PROGRAM: u32 = 3;
 const E_INVALID_INSTRUCTION_DATA_LENGTH: u32 = 4;
 const E_INSUFFICIENT_LAMPORTS: u32 = 5;
+const E_SENDER_DATA_LENGTH_NONZERO: u32 = 6;
+const E_RECIPIENT_DATA_LENGTH_NONZERO: u32 = 7;
+
+enum AccountPosition {
+    Sender = 0,
+    Recipient = 1,
+    SystemProgram = 2,
+}
 
 const TRANSFER_AMOUNT: u64 = 10;
 
@@ -24,10 +32,9 @@ fn test_asm() {
     let system_meta = AccountMeta::new_readonly(system_program, false);
     let sender_pubkey = Pubkey::new_unique();
     let sender_meta = AccountMeta::new(sender_pubkey, true);
-    let sender_account = Account::new(0, 0, &system_program);
     let recipient_pubkey = Pubkey::new_unique();
     let recipient_meta = AccountMeta::new(recipient_pubkey, false);
-    let recipient_account = Account::new(0, 0, &system_program);
+    let mut recipient_account = Account::new(0, 0, &system_program);
 
     // Check no accounts passed.
     let mut instruction = Instruction::new_with_bytes(setup.program_id, &[], vec![]);
@@ -37,17 +44,31 @@ fn test_asm() {
         &[Check::err(ProgramError::Custom(E_N_ACCOUNTS))],
     );
 
-    // Check duplicate recipient account.
+    // Check nonzero sender data length.
+    let mut sender_account = Account::new(0, 1, &system_program);
     instruction.accounts = vec![
         sender_meta.clone(),
-        sender_meta.clone(),
+        recipient_meta.clone(),
         system_meta.clone(),
     ];
     let mut accounts = vec![
         (sender_pubkey, sender_account.clone()),
-        (sender_pubkey, sender_account.clone()),
+        (recipient_pubkey, recipient_account.clone()),
         (system_program, system_account.clone()),
     ];
+    setup.mollusk.process_and_validate_instruction(
+        &instruction,
+        &accounts,
+        &[Check::err(ProgramError::Custom(
+            E_SENDER_DATA_LENGTH_NONZERO,
+        ))],
+    );
+
+    // Check duplicate recipient account.
+    sender_account.data = vec![];
+    instruction.accounts[AccountPosition::Recipient as usize] = sender_meta.clone();
+    accounts[AccountPosition::Sender as usize] = (sender_pubkey, sender_account.clone());
+    accounts[AccountPosition::Recipient as usize] = (sender_pubkey, sender_account.clone());
     setup.mollusk.process_and_validate_instruction(
         &instruction,
         &accounts,
@@ -56,17 +77,24 @@ fn test_asm() {
         ))],
     );
 
+    // Check nonzero recipient data length.
+    recipient_account.data = vec![0];
+    instruction.accounts[AccountPosition::Recipient as usize] = recipient_meta.clone();
+    accounts[AccountPosition::Recipient as usize] = (recipient_pubkey, recipient_account.clone());
+    setup.mollusk.process_and_validate_instruction(
+        &instruction,
+        &accounts,
+        &[Check::err(ProgramError::Custom(
+            E_RECIPIENT_DATA_LENGTH_NONZERO,
+        ))],
+    );
+
     // Check duplicate system program account.
-    instruction.accounts = vec![
-        sender_meta.clone(),
-        recipient_meta.clone(),
-        sender_meta.clone(),
-    ];
-    accounts = vec![
-        (sender_pubkey, sender_account.clone()),
-        (recipient_pubkey, recipient_account.clone()),
-        (system_program, system_account.clone()),
-    ];
+    recipient_account.data = vec![];
+    instruction.accounts[AccountPosition::SystemProgram as usize] = recipient_meta.clone();
+    accounts[AccountPosition::Recipient as usize] = (recipient_pubkey, recipient_account.clone());
+    accounts[AccountPosition::SystemProgram as usize] =
+        (recipient_pubkey, recipient_account.clone());
     setup.mollusk.process_and_validate_instruction(
         &instruction,
         &accounts,
@@ -125,9 +153,11 @@ fn test_offsets() {
     // Sender.
     const SENDER_OFFSET: usize = 8;
     const SENDER_LAMPORTS_OFFSET: usize = 80;
+    const SENDER_DATA_LENGTH_OFFSET: usize = 88;
 
     // Recipient.
     const RECIPIENT_OFFSET: usize = 10344;
+    const RECIPIENT_DATA_LENGTH_OFFSET: usize = 10424;
 
     // System program.
     const SYSTEM_PROGRAM_OFFSET: usize = 20680;
@@ -141,8 +171,16 @@ fn test_offsets() {
         SENDER_OFFSET + offset_of!(StandardAccount, lamports),
     );
     assert_eq!(
+        SENDER_DATA_LENGTH_OFFSET,
+        SENDER_OFFSET + offset_of!(StandardAccount, data_length),
+    );
+    assert_eq!(
         RECIPIENT_OFFSET,
         SENDER_OFFSET + size_of::<StandardAccount>()
+    );
+    assert_eq!(
+        RECIPIENT_DATA_LENGTH_OFFSET,
+        RECIPIENT_OFFSET + offset_of!(StandardAccount, data_length),
     );
     assert_eq!(
         SYSTEM_PROGRAM_OFFSET,
