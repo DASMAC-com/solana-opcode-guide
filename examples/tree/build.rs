6f098608ab140497f0d64858d30147aa1f81d083
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2026-02-03 10:33:05 -0800

    Clean up layout

--- examples/tree/build.rs
+++ examples/tree/build.rs
@@ -1,22 +1,21 @@
 use std::{fs, path::Path};
 
+const ENTRYPOINT_START: &str = ".globl entrypoint";
+
 fn main() {
-    // Inject constants into ASM file.
+    // Read in the assembly file and find the entrypoint marker.
     let manifest_dir = env!("CARGO_MANIFEST_DIR");
     let asm_path = Path::new(manifest_dir).join("src/tree/tree.s");
-
-    const MARKER: &str = ".globl entrypoint";
-
     let content = fs::read_to_string(&asm_path).unwrap();
-    let marker_pos = content.find(MARKER).unwrap();
+    let marker_pos = content.find(ENTRYPOINT_START).unwrap();
 
+    // Generate the constants and insert them before the entrypoint marker.
     let constants = format!(
         "{}\n{}",
         interface::memory_map::to_asm(),
-        interface::ErrorCodes::to_asm()
+        interface::ErrorCodes::to_asm(),
     );
-    let new_content = format!("{}{}", constants, &content[marker_pos..]);
-
+    let new_content = format!("{}\n{}", constants, &content[marker_pos..]);
     if new_content != content {
         fs::write(&asm_path, new_content).unwrap();
     }
--- examples/tree/src/tree/tree.s
+++ examples/tree/src/tree/tree.s
@@ -6,6 +6,7 @@
 # ------------
 .equ E_N_ACCOUNTS, 1 # An invalid number of accounts were passed.
 .equ E_USER_DATA, 2 # The user account has nonzero data length.
+
 .globl entrypoint
 
 entrypoint:
