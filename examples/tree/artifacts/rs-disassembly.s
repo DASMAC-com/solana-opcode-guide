f25eb80e7467583a7c2143b5913bd9d8a70fa817
Alex Kahn <43892045+alnoki@users.noreply.github.com>
2026-02-05 17:55:30 -0800

    Update match

--- examples/tree/artifacts/rs-disassembly.s
+++ examples/tree/artifacts/rs-disassembly.s
@@ -2,18 +2,9 @@
 
 entrypoint:
   ldxdw r2, [r1+0]
-  jne r2, 2, jmp_0038
-  ldxdw r1, [r1+88]
-  mov64 r0, 6677
-  jeq r1, 67, jmp_0030
-  mov64 r0, 666777
-
-jmp_0030:
-  exit
-
-jmp_0038:
+  jeq r2, 2, jmp_0098
   mov64 r0, 1
-  jne r2, 3, jmp_0030
+  jne r2, 3, jmp_00b8
   ldxdw r2, [r1+88]
   jne r2, 0, jmp_00c0
   ldxb r2, [r1+10344]
@@ -26,26 +17,35 @@ jmp_0038:
   jne r2, 14, jmp_0100
   mov64 r0, 0
   ldxdw r1, [r1+31032]
-  jeq r1, 0, jmp_0030
+  jeq r1, 0, jmp_00b8
   mov64 r0, 7
-  ja jmp_0030
+  ja jmp_00b8
+
+jmp_0098:
+  ldxdw r1, [r1+88]
+  mov64 r0, 6677
+  jeq r1, 67, jmp_00b8
+  mov64 r0, 666777
+
+jmp_00b8:
+  exit
 
 jmp_00c0:
   mov64 r0, 2
-  ja jmp_0030
+  ja jmp_00b8
 
 jmp_00d0:
   mov64 r0, 5
-  ja jmp_0030
+  ja jmp_00b8
 
 jmp_00e0:
   mov64 r0, 3
-  ja jmp_0030
+  ja jmp_00b8
 
 jmp_00f0:
   mov64 r0, 6
-  ja jmp_0030
+  ja jmp_00b8
 
 jmp_0100:
   mov64 r0, 4
-  ja jmp_0030
+  ja jmp_00b8
